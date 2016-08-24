3999a7060982cd9200a0794e3273e3bdc1d68471
Deniz Aydemir <deniz@chatous.com>
2016-08-24 04:10:13 -0700

    Initial Commit

---
+++ iOS Test Obj C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Test Obj C
+//
+//  Created by Deniz Aydemir on 8/24/16.
+//  Copyright © 2016 Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS Test Obj C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Test Obj C
+//
+//  Created by Deniz Aydemir on 8/24/16.
+//  Copyright © 2016 Castle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
